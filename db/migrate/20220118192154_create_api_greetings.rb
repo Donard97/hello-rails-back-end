e520c20c696d4db98c9765677f7aea6ad194a86e
Donard97 <donardgolaj@gmail.com>
2022-01-18 21:20:12 +0100

    Generate greetings table

--- db/migrate/20220118192154_create_api_greetings.rb
+++ db/migrate/20220118192154_create_api_greetings.rb
@@ -1,7 +1,7 @@
 class CreateApiGreetings < ActiveRecord::Migration[7.0]
   def change
     create_table :api_greetings do |t|
-      t.string :index
+      t.string :greeting
 
       t.timestamps
     end
--- db/migrate/20220118192758_create_greetings.rb
+++
@@ -1,8 +0,0 @@
-class CreateGreetings < ActiveRecord::Migration[7.0]
-  def change
-    create_table :greetings do |t|
-
-      t.timestamps
-    end
-  end
-end
