98b0dcd2d5308bf323c671aaf95aec2147a7c77e
Donard97 <donardgolaj@gmail.com>
2022-01-18 21:27:35 +0100

    Delete unexpected end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     resources :greetings, only: [:index]
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
