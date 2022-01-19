adb798fc14fc28363450da4f3d19595a5056f48d
Donard97 <donardgolaj@gmail.com>
2022-01-19 22:21:59 +0100

    Delete unnecessary table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_19_150831) do
+ActiveRecord::Schema.define(version: 2022_01_19_211445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "api_greetings", force: :cascade do |t|
-    t.string "index"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "greetings", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
